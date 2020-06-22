4d1c3c32aff6dbbadd3c970a0fb15ba3de091f08
Carmelo Zavaglia <carmeloz2220@gmail.com>
2020-06-22 21:55:42 +0200

    adding other js

---
+++ js/script.js
@@ -0,0 +1,37 @@
+  // Funzione topscroll
+
+// function topFunction() {
+//   document.body.scrollTop = 0; // For Safari
+//   document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+// }
+
+function scrollToTop(scrollDuration) {
+    var scrollStep = -window.scrollY / (scrollDuration / 15),
+        scrollInterval = setInterval(function(){
+        if ( window.scrollY != 0 ) {
+            window.scrollBy( 0, scrollStep );
+        }
+        else clearInterval(scrollInterval);
+    },15);
+}
+
+// ResizeBar Function
+
+window.onscroll = function() {scrollFunction()};
+function scrollFunction() {
+  if (document.body.scrollTop > 100 || document.documentElement.scrollTop > 100) {
+    document.getElementById("header").style.paddingTop = "0px";
+    document.getElementById("header").style.paddingBottom = "0px";
+  } else {
+    document.getElementById("header").style.paddingTop = "20px";
+    document.getElementById("header").style.paddingBottom = "20px";
+  }
+
+  mybutton = document.getElementById("myBtn");
+  if (document.body.scrollTop > 200 || document.documentElement.scrollTop > 200) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+
+}
