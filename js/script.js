125b6681a6cee68a00da81e7dbaa8fc58ce77329
Carmelo Zavaglia <carmeloz2220@gmail.com>
2020-06-22 22:43:16 +0200

    Complete js

--- js/script.js
+++ js/script.js
@@ -34,4 +34,41 @@ function scrollFunction() {
     mybutton.style.display = "none";
   }
 
+  portfolio = document.getElementById("portfolio");
+  portfolio.classList.remove("active");
+  about = document.getElementById("about");
+  about.classList.remove("active");
+  contact = document.getElementById("contact");
+  contact.classList.remove("active");
+
+  if (document.body.scrollTop > 2059 || document.documentElement.scrollTop > 2059) {
+    contact.classList.add("active");
+} else if (document.body.scrollTop > 1481 || document.documentElement.scrollTop > 1481) {
+    about.classList.add("active");
+} else if(document.body.scrollTop > 600 || document.documentElement.scrollTop > 600) {
+    portfolio.classList.add("active");
+  }
 }
+
+// (function() {
+//   'use strict';
+//
+//   var section = document.querySelectorAll(".section-spy");
+//   var sections = {};
+//   var i = 0;
+//
+//   Array.prototype.forEach.call(section, function(e) {
+//     sections[e.id] = e.offsetTop;
+//   });
+//
+//   window.onscroll = function() {
+//     var scrollPosition = document.documentElement.scrollTop || document.body.scrollTop;
+//
+//     for (i in sections) {
+//       if (sections[i] <= scrollPosition) {
+//         document.querySelector('.active').setAttribute('class', ' ');
+//         document.querySelector('a[href*=' + i + ']').setAttribute('class', 'active');
+//       }
+//     }
+//   };
+// })();
